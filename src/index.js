7a721c02958868e19e9582ab883d3df03772f15a
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-05-29 00:57:14 -0700

    pass third test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 export class CalcAge{
   ageMercury(input){
     this.age = input * .24;
+    this.age = Math.round(input * .24);
     console.log(this.age);
     return this.age;
   }
   ageVenus(input){
     this.age = input * .62;
+    this.age = Math.round(input * .62);
     console.log(this.age);
     return this.age;
   }
-} 
+  ageMars(input){
+    this.age = Math.round(input * 1.88);
+    console.log(this.age);
+    return this.age;
+  }
+}
