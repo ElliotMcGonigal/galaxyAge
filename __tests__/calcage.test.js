45e2c7ba78e734ac77cfcf968a0ddb57d1110d26
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-05-29 01:00:35 -0700

    add fourth test and fix math in main program

--- __tests__/calcage.test.js
+++ __tests__/calcage.test.js
@@ -3,8 +3,9 @@ import { CalcAge } from "./../src/index.js/";
 describe('CalcAge', ()=>{
   test('should return inputted as as their age on Mercury', ()=>{
     const age = new CalcAge(23);
-    expect(age.ageMercury(23)).toEqual(6);
-    expect(age.ageVenus(23)).toEqual(14);
-    expect(age.ageMars(23)).toEqual(43);
+    expect(age.ageMercury(23)).toEqual(96);
+    expect(age.ageVenus(23)).toEqual(37);
+    expect(age.ageMars(23)).toEqual(12);
+    expect(age.ageJupiter(23)).toEqual(2);
   });
 }); 
