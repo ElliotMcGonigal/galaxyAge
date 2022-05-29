70787dd6555c22ccdd85397b439ad045cd89ef68
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-05-29 01:18:21 -0700

    add tests for lifeleft

--- __tests__/calcage.test.js
+++ __tests__/calcage.test.js
@@ -1,11 +1,22 @@
-import { CalcAge } from "./../src/index.js/";
+import { CalcAge } from "./../src/calcage.js";
+import { LifeLeft } from "./../src/lifeleft.js";
 
 describe('CalcAge', ()=>{
-  test('should return inputted as as their age on Mercury', ()=>{
+  test('should return inputted number as their age on Mercury', ()=>{
     const age = new CalcAge(23);
     expect(age.ageMercury(23)).toEqual(96);
     expect(age.ageVenus(23)).toEqual(37);
     expect(age.ageMars(23)).toEqual(12);
     expect(age.ageJupiter(23)).toEqual(2);
+  });
+}); 
+
+describe('LifeLeft', ()=>{
+  test('should return their remaining years on Mercury, Venus, Mars, and Jupiter', ()=>{
+    const remain = new LifeLeft(23);
+    expect(remain.leftMercury(23)).toEqual(232);
+    expect(remain.leftVenus(23)).toEqual(90);
+    expect(remain.leftMars(23)).toEqual(30);
+    expect(remain.leftJupiter(23)).toEqual(5);
   });
 }); 
