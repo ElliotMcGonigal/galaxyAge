e305159bf4193897aa3a826c80199db4648cf9ae
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-05-29 00:28:36 -0700

    pass first test

--- __tests__/calcage.test.js
+++ __tests__/calcage.test.js
@@ -2,8 +2,8 @@ import { TestScheduler } from "jest";
 import { CalcAge } from "./../src/index.js/";
 
 describe('CalcAge', ()=>{
-  TestScheduler('should return inputted as as their age on Mercury', ()=>{
+  test('should return inputted as as their age on Mercury', ()=>{
     const age = new CalcAge(23);
-    expect(age.ageMercury).toEqual(5.52);
+    expect(age.ageMercury(23)).toEqual(5.52);
   });
 }); 
---
+++ src/index.js
@@ -0,0 +1,7 @@
+export class CalcAge{
+  ageMercury(input){
+    this.age = input * .24;
+    console.log(this.age);
+    return this.age;
+  }
+} 
