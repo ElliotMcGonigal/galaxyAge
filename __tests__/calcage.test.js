fbcb43c33a402de1224f0d9eacd5e1e8b2063645
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-05-29 00:51:28 -0700

    add third test

--- __tests__/calcage.test.js
+++ __tests__/calcage.test.js
@@ -5,5 +5,6 @@ describe('CalcAge', ()=>{
     const age = new CalcAge(23);
     expect(age.ageMercury(23)).toEqual(5.52);
     expect(age.ageVenus(23)).toEqual(14.26);
+    expect(age.ageMars(23)).toEqual(43.24);
   });
 }); 
